e30bc8af162f528638391e939c8760453e2f0583
vy <victoriaalfarofernandez@gmail.com>
2024-10-25 15:57:08 -0300

    Update Tienda.php

--- pipi/Tienda.php
+++ pipi/Tienda.php
@@ -52,7 +52,7 @@ session_start();
 <body>
     <header>
         <div class="logo">
-            <img src="logo.png" href="Frenchi.html" style="width:135px;height: 80px;">
+            <img src="Imagenes/logo.png" href="Frenchi.html" style="width:135px;height: 80px;">
         </div>
         <nav>
             <ul>
@@ -66,7 +66,7 @@ session_start();
                     echo "<a class='login'>".$_SESSION['usuario']."</a>";
                 }
                 else{
-                    echo "<a class='login' href='Login.html'>Crear cuenta</a>";
+                    echo "<a class='login' href='Registro.html'>Crear cuenta</a>";
                 } ?>         
                 </li>
                 
@@ -80,265 +80,35 @@ session_start();
         <section class="container top-categories">
             <h2>Lo mejor de Frenchí cada vez más cerca tuyo...</h2>
             <div class="container-categories">
-                <div class="card-category category-desayuno">
-                    <p><a href="Desayunos.html">Desayunos y Meriendas</a></p>
-                    
-                </div>
-                <div class="card-category category-brunch">
-                <p><a href="Brunch.html">Brunch</a></p>
-                    
-                    
-                </div>
-            </div>
-        </section>
-
-        <section class="container top-products">
-            <h1 class="heading-1">Nuestros favoritos</h1>
-
-
-            <div class="container-products">
-                <!-- Producto 1 -->
-                <?php
-                    while ($fila = mysqli_fetch_assoc($resultado1)){ ?>
-                        <div class="card-product">
-                        <div class="container-img">
-                            <img src="<?php echo $fila['imagen']?>" alt="Cafe Irish" />
-                           
-                        
-                            <div class="button-group">
-                              
-                                <span>
-                                    <i class="fa-regular fa-heart"></i>
-                                </span>
-                                <li><a class="login" href="Login.html">Crear cuenta</a></li>
-                            </div>
-                        </div>
-                        <div class="content-card-product">
-                            <div class="stars">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <h3><?php echo $fila["nombre"]?></h3>
-                            <span class="add-cart">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </span>
-                            <p class="price">$<?php echo $fila["precio"]?></p>
-    
-                        </div>
-                    </div>
-                    <?php } ?>
-            </div>
-        </section>
-
-
-        <section class="gallery">
-            <img
-                src="https://i.pinimg.com/736x/de/4e/ba/de4eba837959e139d0273e450cdbb8bb.jpg"
-                alt="Gallery Img1"
-                class="gallery-img-1"
-            />
-            <img
-                src="https://i.pinimg.com/564x/d4/17/d7/d417d7fcfc8df95961b5be8abfb7349f.jpg"
-                alt="Gallery Img2"
-                class="gallery-img-2"
-            />
-            <img
-            src="https://i.pinimg.com/736x/2a/e5/5c/2ae55c154dfd85acf2dd9502d384520e.jpg"
-                alt="Gallery Img3"
-                class="gallery-img-3"
-            />
-            <img
-                src="https://i.pinimg.com/564x/e8/1c/21/e81c21fefbdf7b081cd8da3195aab829.jpg"
-                alt="Gallery Img4"
-                class="gallery-img-4"
-            />
-            <img
-                src="https://i.pinimg.com/564x/c1/ee/a9/c1eea91dad4e5432d9a6008535970dff.jpg"
-                alt="Gallery Img5"
-                class="gallery-img-5"
-            />
-        </section>
-
-        <section class="container specials">
-            <h1 class="heading-1">Bebidas</h1>
-                    
-            <div class="container-products">
-            <?php
-                    while ($fila = mysqli_fetch_assoc($resultado2)){ ?>
-                        <div class="card-product">
-                        <div class="container-img">
-                            <img src="<?php echo $fila['imagen']?>" alt="Cafe Irish" />
-                            
-                            <div class="button-group">
-                              
-                                <span>
-                                    <i class="fa-regular fa-heart"></i>
-                                </span>
-                           
-                            </div>
-                        </div>
-                        <div class="content-card-product">
-                            <div class="stars">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <h3><?php echo $fila["nombre"]?></h3>
-                            <span class="add-cart">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </span>
-                            <p class="price">$<?php echo $fila["precio"]?></p>
-    
-                        </div>
-                    </div>
-                    <?php } ?>
-            </div>
-        </section>
-        <section class="container specials">
-            <h1 class="heading-1">Patisserie</h1>
-                    
-            <div class="container-products">
-            <?php
-                    while ($fila = mysqli_fetch_assoc($resultado3)){ ?>
-                        <div class="card-product">
-                        <div class="container-img">
-                            <img src="<?php echo $fila['imagen']?>" alt="Cafe Irish" />
-                            
-                            <div class="button-group">
-                              
-                                <span>
-                                    <i class="fa-regular fa-heart"></i>
-                                </span>
-                           
-                            </div>
-                        </div>
-                        <div class="content-card-product">
-                            <div class="stars">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <h3><?php echo $fila["nombre"]?></h3>
-                            <h4><?php echo $fila["descripcion"]?></h4>
-                            <span class="add-cart">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </span>
-                            <p class="price">$<?php echo $fila["precio"]?></p>
-    
-                        </div>
-                    </div>
-                    <?php } ?>
-            </div>
-        </section>
-        <section class="container specials">
-            <h1 class="heading-1">Comidas</h1>
-                    
-            <div class="container-products">
-            <?php
-                    while ($fila = mysqli_fetch_assoc($resultado4)){ ?>
-                        <div class="card-product">
-                        <div class="container-img">
-                            <img src="<?php echo $fila['imagen']?>" alt="Cafe Irish" />
-                            
-                            <div class="button-group">
-                              
-                                <span>
-                                    <i class="fa-regular fa-heart"></i>
-                                </span>
-                           
-                            </div>
-                        </div>
-                        <div class="content-card-product">
-                            <div class="stars">
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-solid fa-star"></i>
-                                <i class="fa-regular fa-star"></i>
-                            </div>
-                            <h3><?php echo $fila["nombre"]?></h3>
-                            <span class="add-cart">
-                                <i class="fa-solid fa-basket-shopping"></i>
-                            </span>
-                            <p class="price">$<?php echo $fila["precio"]?></p>
-    
-                        </div>
-                    </div>
-                    <?php } ?>
-            </div>
-        </section>
-    </main>
-    <footer>
-        <li><b href="#">Instagram</b></li>
-        <li><b href="#">Facebook</b></li>
-        <li><b href="#">Maps</b></li>
-    <p>©Copyright 2024 de Cafeterías Frenchí. Todos los derechos reservados.</p>
-    </footer>
-</body>
-</html>
-
-
-        $query2="select nombre, descripcion, precio, imagen, calificacion from Productos where tipo_comida='Bebida'";
-        $resultado2=mysqli_query($conexion, $query2);
-        $query3="select nombre, descripcion, precio, imagen, calificacion from Productos where tipo_comida='Comida' and tipo_ds='Dulce'";
-        $resultado3=mysqli_query($conexion, $query3);    
-        $query4="select nombre, descripcion, precio, imagen, calificacion from Productos where tipo_comida='Comida' and tipo_ds='Salado'";
-        $resultado4=mysqli_query($conexion, $query4);    
-    
-    }
-        ?>
-<!doctype html>
-<html>
-<head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-    <meta charset="utf-8" />
-    <title>Tienda</title>
-    <link
-    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@0;1&display=swap" rel="stylesheet">
-    <link href="https://fonts.googleapis.com/css2?family=Sevillana&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="Tienda_style.css" />
-    <link rel="icon" type="image/png" href="https://png.pngtree.com/png-vector/20220723/ourmid/pngtree-fresh-croissant-icon-cartoon-style-with-shadow-free-vector-and-png-png-image_6034269.png">
-</head>
-
-<body>
-    <header>
-        <div class="logo">
-            <img src="Imagenes/logo.png" href="Frenchi.html" style="width:135px;height: 80px;">
-        </div>
-        <nav>
-            <ul>
-                <li><a href="Nosotros.html">Nosotros</a></li>
-                <li><a href="Menu.html">Menú</a></li>
-                <li><a href="Locales.html">Locales</a></li>
-                <li><a href="Tienda.html">Tienda</a></li>
-                <li><a class="login" href="Login.html">Crear cuenta</a></li>
                 
-            </ul>
-        </nav>
-    </header>
-    <div class="banner-area"></div>
-    
-    
-    <main>
-        <section class="container top-categories">
-            <h2>Lo mejor de Frenchí cada vez más cerca tuyo...</h2>
-            <div class="container-categories">
-                <div class="card-category category-desayuno">
+            <div class="card-category category-desayuno">
                     <p><a href="Desayunos.html">Desayunos y Meriendas</a></p>
                     
                 </div>
-                <div class="card-category category-brunch">
-                <p><a href="Brunch.html">Brunch</a></p>
+                <div class="card-category category-brunch">777
+                    <p><a href="Brunch.html">Brunch</a></p>
+                </div>
+                <div class="card-category category-almuerzo">
+                    <p><a href="Desayunos.html">Desayunos y Meriendas</a></p>
                     
+                </div>
+                <div class="card-category category-desayunotexto">
+                    <p><a href="Desayunos.html">Desayunos y Meriendas</a></p>
                     
+                </div>
+                
+                <div class="card-category category-brunchtexto">
+                    <p><a href="Brunch.html">Brunch</a></p>
+                </div>
+
+                
+                <div class="card-category category-almuerzotexto">
+                    <p><a href="Desayunos.html">Desayunos y Meriendas</a></p>
+                    
+                </div>
+                
+            
+
                 </div>
             </div>
         </section>
@@ -359,9 +129,9 @@ session_start();
                             <div class="button-group">
                               
                                 <span>
-                                    <i class="fa-regular fa-heart"></i>
+                                    <i class="fa-solid fa-heart"></i>
                                 </span>
-                                <li><a class="login" href="Login.html">Crear cuenta</a></li>
+                                
                             </div>
                         </div>
                         <div class="content-card-product">
@@ -426,7 +196,7 @@ session_start();
                             <div class="button-group">
                               
                                 <span>
-                                    <i class="fa-regular fa-heart"></i>
+                                    <i class="fa-solid fa-heart"></i>
                                 </span>
                            
                             </div>
@@ -463,7 +233,7 @@ session_start();
                             <div class="button-group">
                               
                                 <span>
-                                    <i class="fa-regular fa-heart"></i>
+                                    <i class="fa-solid fa-heart"></i>
                                 </span>
                            
                             </div>
@@ -477,7 +247,7 @@ session_start();
                                 <i class="fa-regular fa-star"></i>
                             </div>
                             <h3><?php echo $fila["nombre"]?></h3>
-                            <h4><?php echo $fila["descripcion"]?></h4>
+                            <p class="descripcion"><?php echo $fila["descripcion"]?></p>
                             <span class="add-cart">
                                 <i class="fa-solid fa-basket-shopping"></i>
                             </span>
@@ -501,7 +271,7 @@ session_start();
                             <div class="button-group">
                               
                                 <span>
-                                    <i class="fa-regular fa-heart"></i>
+                                    <i class="fa-solid fa-heart"></i>
                                 </span>
                            
                             </div>
