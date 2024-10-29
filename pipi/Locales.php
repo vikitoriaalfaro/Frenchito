0fcc2120c06d423b18c0535c9b1f4914a9e97f21
vy <victoriaalfarofernandez@gmail.com>
2024-10-29 17:41:15 -0300

    Update Locales.php

--- pipi/Locales.php
+++ pipi/Locales.php
@@ -7,7 +7,7 @@ session_start();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>Frenchí Cafetería</title>
+    <title>Locales</title>
     <link
     href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@0;1&display=swap" rel="stylesheet">
     
@@ -135,3 +135,4 @@ session_start();
     <script src="Carrusel.js"></script>
 </body>
 </html>
+
