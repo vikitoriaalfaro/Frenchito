bb51594d410b4a221701262a07f1ed61b2b8e023
vy <victoriaalfarofernandez@gmail.com>
2024-11-01 15:47:01 -0300

    Add files via upload

---
+++ pipi/Menu.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+?>
+<!doctype html>
+<html>
+  <head>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <meta charset="utf-8" />
+
+    <title>Menu</title>
+    <link
+    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@0;1&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Sevillana&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet">
+  
+
+      
+    <link rel="stylesheet" href="Menu.css" />
+    <link rel="icon" type="image/png" href="https://png.pngtree.com/png-vector/20220723/ourmid/pngtree-fresh-croissant-icon-cartoon-style-with-shadow-free-vector-and-png-png-image_6034269.png">
+  </head>
+  <body>
+    
+      <header>
+        <div class="logo">
+          <img src="Imagenes/logo.png" style="width:135px;height: 80px;">
+        </div>
+        <nav>
+          <ul>
+            <li><a href="Nosotros.php">Nosotros</a></li>
+            <li><a href="Menu.php">Menú</a></li>
+            <li><a href="Locales.php">Locales</a></li>
+            <li><a href="Tienda.php">Tienda</a></li>
+            <li>
+              <?php
+              if(isset($_SESSION["usuario"])){
+                    echo "<a class='login' href='logout.php'>".$_SESSION['usuario']."</a>";
+                }
+                else{
+                    echo "<a class='login' href='Registro.html'>Crear cuenta</a>";
+                } ?>         
+                </li>
+
+          </ul>
+        </nav>
+      </header>
+      <div class="banner-area"></div>
+   
+   
+     <main>
+        <div class="menu">
+        <img class="fotito" src="Imagenes/Menu.jpeg">
+          
+          </div>
+         
+          
+     </main>
+    <footer>
+
+    <li><a href="#">Instagram</a></li>
+        <li><a href="#">Tel:15652783</a></li>
+        <li><a href="https://open.spotify.com/playlist/3lywTMc9iYn1J9lsKEJ5fA?si=lOhatp24TA6T60sgn-f8sA&pt=513b45ecc5e58a426401db54352cba7d&pi=2RRkj6mTRoyLJ">Playlist de Spotify</a></li>
+      <p>©Copyright 2024 de Cafeterías Frenchí. Todos los derechos reservados.</p>
+    </footer>
+  </body>
+</html>
+
+
+
+
+
+
