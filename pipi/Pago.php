e14f2c5facdd71f4d317f42888f6b1f28a62afa7
vy <victoriaalfarofernandez@gmail.com>
2024-11-01 15:44:29 -0300

    Add files via upload

---
+++ pipi/Pago.php
@@ -0,0 +1,79 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">    
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>Locales</title>
+    <link
+    href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital@0;1&display=swap" rel="stylesheet">
+    
+    <link href="https://fonts.googleapis.com/css2?family=Oooh+Baby&display=swap" rel="stylesheet">
+    <link rel="icon" type="image/png" href="f.png">
+    <link rel="stylesheet" href="Pago_styles.css">
+    <link rel="icon" type="image/png" href="https://png.pngtree.com/png-vector/20220723/ourmid/pngtree-fresh-croissant-icon-cartoon-style-with-shadow-free-vector-and-png-png-image_6034269.png">
+
+</head>
+
+<body>
+ 
+        <header>
+          <div class="logo">
+            <img src="Imagenes/logo.png" style="width:135px;height: 80px;">
+          </div>
+         
+        </header>
+      <main>
+      
+<div class="card">
+  <div class="card-wrapper">
+    <div class="card-icon">
+      <div class="icon-cart-box">
+        <svg
+          viewBox="0 0 576 512"
+          width="20"
+          height="20"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"
+            fill="#009688"
+          ></path>
+        </svg>
+      </div>
+    </div>
+
+    <div class="card-content">
+      <div class="card-title-wrapper">
+        <span class="card-title">Compra exitosa!</span>
+        <span class="card-action">
+         
+            <path
+              d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z"
+            ></path>
+          </svg>
+        </span>
+      </div>
+      <div class="product-name">Se enviará a su dirección en...</div>
+      <div class="product-price">20-30 minutos</div>
+      <a href="Tienda.php">
+      <button class="btn-view-cart" type="button">Volver a la Tienda</button>
+      </a>
+    </div>
+  </div>
+</div>
+
+    </main>
+    <footer>
+
+    <li><a href="#">Instagram</a></li>
+        <li><a href="#">Tel:15652783</a></li>
+        <li><a href="https://open.spotify.com/playlist/3lywTMc9iYn1J9lsKEJ5fA?si=lOhatp24TA6T60sgn-f8sA&pt=513b45ecc5e58a426401db54352cba7d&pi=2RRkj6mTRoyLJ">Playlist de Spotify</a></li>
+    <p>©Copyright 2024 de Cafeterías Frenchí. Todos los derechos reservados.</p>
+    </footer>
+    <script src="Carrusel.js"></script>
+</body>
+</html>
